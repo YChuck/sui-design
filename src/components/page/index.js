f1da4635f417e29a6a0726a033fbde46de74b2ec
YChuck <ychuck19950510@gmail.com>
2019-12-09 15:48:59 +0800

    fix(page): fix page component's params empty bug
    fix the bug when required parammeter is not passed in

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -11,7 +11,7 @@ const state = Vue.observable({
   // 具体页数
   currentPage: false,
   // 每页条数
-  pageSize: 10,
+  pageSize: 0,
   // 总数
   total: 0,
 })
@@ -26,30 +26,37 @@ export default {
     // input 事件监听 (为了实现 v-model)
     const input = ctx.listeners['input']
     const onChange = ctx.listeners['on-change']
-    let value = ctx.data.attrs.value || 1
 
     state.total = ctx.props.total
-    state.pageSize = ctx.props.pageSize
+    // 未设置则使用默认 10 条
+    state.pageSize = ctx.props.pageSize || 10
 
-    // 未记录当前页码 则使用传入 value 值
-    if (!state.currentPage) state.currentPage = value
-    // 若已记录当前页码 则触发 input 事件
-    else if (input) input(state.currentPage)
+    // 未记录当前页码 则使用传入 value 或 默认值
+    if (!state.currentPage)
+      state.currentPage = (ctx.data.attrs && ctx.data.attrs.value) || 1
 
     // current 参数是为了设置 iview page 组件选中
-    ctx.data.attrs.current = state.currentPage
+    ctx.props.current = state.currentPage
 
-    // 监听 iview page 组件的 on-change 事件
-    ctx.data.on['on-change'] = function(page) {
-      // 设置当前选中
-      state.currentPage = page
-      if (onChange) onChange(page)
+    if (ctx.data.on) {
+      // 拦截 iview page 组件的 on-change 事件: 用于触发 v-model 变化、change 事件以及改变当前选中页码
+      ctx.data.on['on-change'] = function(page) {
+        // 设置当前选中
+        state.currentPage = page
+        if (onChange) onChange(page)
+        if (input) input(page)
+      }
     }
 
     return h('div', { class: prefixCls }, [
-      h(Page, {
-        ...ctx.data,
-      }),
+      h(
+        Page,
+        {
+          props: ctx.props,
+          on: ctx.listeners,
+        },
+        ctx.children,
+      ),
       h('input', {
         class: `${prefixCls}-input`,
         on: {
@@ -77,7 +84,13 @@ export default {
               state.currentPage === state.inputNumber || !state.inputNumber,
           },
           on: {
+            /**
+             * 用于触发 v-model 变化、change 事件以及改变当前选中页码
+             */
             click() {
+              const { inputNumber } = state
+              if (input) input(inputNumber)
+              if (onChange) onChange(inputNumber)
               state.currentPage = state.inputNumber
             },
           },
