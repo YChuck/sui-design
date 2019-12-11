aaa7f2e84972a2259c5086ac63d05ac9a50a15a0
YChuck <ychuck19950510@gmail.com>
2019-12-11 17:24:43 +0800

    fix(select): replace functional to normal comp to fix responsive bug

--- src/components/select/index.js
+++ src/components/select/index.js
@@ -1,18 +1,19 @@
 import Vue from 'vue'
 import { Select } from 'iview'
 import Icon from '../icon'
-import { oneOf, shallowCopy } from '../../utils'
+import { oneOf } from '../../utils'
 
 const prefixCls = 'sui-select'
 
-// 标记响应化
-const state = Vue.observable({
-  close: false,
-})
-
 export default {
   name: 'SxSelect',
-  functional: true,
+  // 禁用特性继承
+  inheritAttrs: false,
+  data() {
+    return {
+      close: false,
+    }
+  },
   props: {
     // UI展示级别: global fragment form
     level: {
@@ -22,45 +23,45 @@ export default {
       default: 'fragment',
     },
   },
-  render(h, ctx) {
-    const { level } = ctx.props
-    const { staticStyle = {}, staticClass = {} } = ctx.data
-    /**
-     * 经测试使用 v-model 后,覆盖input方法会使 Vue 处理多个事件 (保留原事件处理函数)
-     * input事件处理函数将通过数组保存, Vue 的 事件处理 invoker 支持数组处理
-     */
-    if (!ctx.data.on) ctx.data.on = {}
-    ctx.data.on['input'] = function(v) {
-      state.close = !!v
-    }
-    return h(
-      'div',
-      { class: [prefixCls, `${prefixCls}-${level}`], staticStyle, staticClass },
-      [
-        h(
-          Select,
-          {
-            ...shallowCopy(ctx.data, ['staticStyle', 'staticClass']),
-            props: { clearable: true },
+  render(h) {
+    const { level } = this
+    return h('div', { class: [prefixCls, `${prefixCls}-${level}`] }, [
+      h(
+        Select,
+        {
+          props: { clearable: true, ...this.$attrs },
+          on: {
+            ...this.$listeners,
+            /**
+             * 经测试使用 v-model 后,覆盖input方法会使 Vue 处理多个事件 (保留原事件处理函数)
+             * input事件处理函数将通过数组保存, Vue 的 事件处理 invoker 支持数组处理
+             */
+            input: v => {
+              this.close = !!v
+            },
           },
-          ctx.children,
-        ),
-        h('div', { class: `${prefixCls}-icon` }, [
-          h(Icon, {
-            props: {
-              type: state.close
-                ? 'icon-error-circle'
-                : level === 'fragment'
-                ? 'icon-arraw-vertical'
-                : 'icon-arrow-down',
-            },
-            style: {
-              width: level === 'fragment' && !state.close ? '16px' : '12px',
-              height: level === 'fragment' && !state.close ? '16px' : '12px',
-            },
-          }),
-        ]),
-      ],
-    )
+          /**
+           * 将封装组件的作用域插槽传入 iView Select 组件
+           */
+          scopedSlots: this.$scopedSlots,
+        },
+        this.$slots.default,
+      ),
+      h('div', { class: `${prefixCls}-icon` }, [
+        h(Icon, {
+          props: {
+            type: this.close
+              ? 'icon-error-circle'
+              : level === 'fragment'
+              ? 'icon-arraw-vertical'
+              : 'icon-arrow-down',
+          },
+          style: {
+            width: level === 'fragment' && !this.close ? '16px' : '12px',
+            height: level === 'fragment' && !this.close ? '16px' : '12px',
+          },
+        }),
+      ]),
+    ])
   },
 }
