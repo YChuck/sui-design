b53e8da919b3c714678da63bb8396a323af47956
YChuck <ychuck19950510@gmail.com>
2019-12-12 10:42:10 +0800

    fix(header): fix the functional comp's class & style set error

--- src/constants.js
+++ src/constants.js
@@ -30,6 +30,7 @@ const SMALL_SIZE = '14'
 const NORMAL_SIZE = '16'
 const LARGE_SIZE = '18'
 const LARGER_SIZE = '20'
+const WITHOUT_ATTRS = ['class', 'staticClass', 'style', 'staticStyle']
 
 export {
   CLASS_PREFIX,
@@ -56,4 +57,5 @@ export {
   NORMAL_SIZE,
   LARGE_SIZE,
   LARGER_SIZE,
+  WITHOUT_ATTRS,
 }
