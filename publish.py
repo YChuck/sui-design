2e9afd0521e354650703d9d66295a2f0df6f5194
YChuck <ychuck19950510@gmail.com>
2019-12-24 11:37:10 +0800

    build(global): rebuild publish.py to publish demo website

--- publish.py
+++ publish.py
@@ -3,21 +3,21 @@ from fabric.api import *
 import os
 
 # fab run_208 publish_208  -f ./publish.py
-# @task
-# def run_208():
-#     env.hosts = ['192.168.199.208']
-#     env.user = 'sxwl1080'
-#     env.password = 'sx'
+@task
+def run_208():
+    env.hosts = ['192.168.199.5']
+    env.user = 'sxkj'
+    env.password = 'sx'
 
-# @task
-# def publish_208():
-#     NGNIX_PATH_208 = '/home/sxwl1080/yangxiang/nginx/html'
+@task
+def publish_208():
+    NGNIX_PATH_208 = '/home/sxkj/yangxiang/sui-design/html'
 
-#     local('npm run build')
-#     local('tar cvzf dist.tgz dist')
-#     with cd(NGNIX_PATH_208):
-#         sudo('rm -rf ./*')
-#         put('dist.tgz', 'dist.tgz')
-#         run('tar xvf dist.tgz')
-#         run('rm -rf dist.tgz')
-#     local('rm -rf publish.pyc dist.tgz')
+    local('npm run build')
+    local('tar cvzf dist.tgz dist')
+    with cd(NGNIX_PATH_208):
+        sudo('rm -rf ./*')
+        put('dist.tgz', 'dist.tgz')
+        run('tar xvf dist.tgz')
+        run('rm -rf dist.tgz')
+    local('rm -rf publish.pyc dist.tgz')
